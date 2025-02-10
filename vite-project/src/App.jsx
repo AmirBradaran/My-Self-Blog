468886ffbe7607aa223c807508ba464d1d514750
Amir <baradaran13085@gmail.com>
2025-02-10 21:54:20 +0330

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Slide from "./Pages/Slide/index";
+import AboutMe from "./Pages/AboutMe/index";
+import ContactMe from "./Pages/ContactMe/index";
+import MyProjects from "./Pages/MyProjects/index";
+import MySkills from "./Pages/MySkills/index";
+import Navbar from "./Components/Navbar/index";
+import Footer from "./Components/Footer/index";
+import React from "react";
+import { Box, Stack } from "@mui/material";
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Stack>
+      <Navbar/>
+      <Box sx={{ p:"0px 10%" }}>
+        <Slide />
+        <AboutMe />
+        <MySkills />
+        <MyProjects />
+        <ContactMe />
+      </Box>
+      <Footer />
+    </Stack>
+  );
 }
